3aae11bef940a081bca5754b3e9a20d32032bcb1
Matt Panaro <matt.panaro@gmail.com>
2014-06-14 10:56:28 -0400

    add TileProperties interface to store constants

---
+++ src/com/mattpanaro/software/android/velocimeter/TileProperties.java
@@ -0,0 +1,18 @@
+package com.mattpanaro.software.android.velocimeter;
+
+public interface TileProperties
+{
+    public static final int TILE_COUNT = 8;
+
+    public static final String[] TILE_NAMES =
+    {
+        "display_elapsed_time",
+        "display_distance",
+        "display_instantaneous_speed",
+        "display_average_speed",
+        "display_numeric_heading",
+        "display_string_heading",
+        "display_latitude",
+        "display_longitude"
+    };
+}
