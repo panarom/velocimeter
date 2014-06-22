8faebab1b36e3302e435bf976e60cd42ff5cee3c
Matt Panaro <matt.panaro@gmail.com>
2014-06-21 23:21:26 -0400

    add LocationService to maintain GPS listening and LocationClient interface that consumes LocationData

---
+++ src/com/mattpanaro/software/android/velocimeter/LocationClient.java
@@ -0,0 +1,6 @@
+package com.mattpanaro.software.android.velocimeter;
+
+public interface LocationClient
+{
+    public void processLocationData(LocationData locationData);
+}
---
+++ src/com/mattpanaro/software/android/velocimeter/LocationService.java
@@ -0,0 +1,59 @@
+package com.mattpanaro.software.android.velocimeter;
+
+import java.util.HashSet;
+
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class LocationService extends android.app.Service
+{
+    private final IBinder binder = new LocationBinder();
+
+    private LocationListener locationListener;
+
+    private HashSet<LocationClient> locationClients = new HashSet<LocationClient>();
+
+    public void onCreate()
+    {
+        locationListener = new LocationListener(this);
+    }
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        locationListener.onStart();
+        return START_STICKY;
+    }
+    public boolean StopService(Intent name)
+    {
+        locationListener.onStop();
+        return true;
+    }
+
+    public void updateLocationData(LocationData locationData)
+    {
+        for(LocationClient client : locationClients) client.processLocationData(locationData);
+    }
+
+    public LocationService addLocationClient(LocationClient locationClient)
+    {
+        locationClients.add(locationClient);
+        return this;
+    }
+    public boolean removeLocationClient(LocationClient locationClient)
+    {
+        return locationClients.remove(locationClient);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public class LocationBinder extends Binder
+    {
+        LocationService getService()
+        {
+            return LocationService.this;
+        }
+    }
+}
